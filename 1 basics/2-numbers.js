4b8f26dfe54a0ffe824d792a4bd7f263ed7a5c5f
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-24 20:35:28 +0300

    numbers

---
+++ 1 basics/2-numbers.js
@@ -0,0 +1,19 @@
+let age = 29
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// CHALLENGE //
+
+// studentScore 18
+// maxScore 20
+// percent 90
+
+// print the percentage
+
+let studentScore = 67;
+let maxScore = 100;
+
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
