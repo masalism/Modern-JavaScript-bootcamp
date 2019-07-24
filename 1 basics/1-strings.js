41c31eeb66d3acf54ac0efea623f465da80a6561
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-24 20:21:08 +0300

    strings and variables

---
+++ 1 basics/1-strings.js
@@ -0,0 +1,13 @@
+let firstName = 'Mantas'
+let lastName = 'Masalis'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+//TEST//
+
+let city = 'Kaunas'
+let country = 'Lietuva'
+let location = city + ', ' + country
+
+console.log(location)
