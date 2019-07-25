8c6b9b7aefb499507252e6659c414765774cc344
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-25 11:18:43 +0300

    temp converter

---
+++ 1 basics/4-temp-conv.js
@@ -0,0 +1,10 @@
+// TEMPERATURE CONVERTER
+let fahrenheit = 78 //0C - 273.15k
+
+// calculate celsius and store in celsius variable
+let celsius = (fahrenheit -32) / 1.8
+console.log(celsius + '°C')
+// calculate kelvin
+// print
+let kelvins = (fahrenheit + 459.67) / 1.8
+console.log(kelvins)
