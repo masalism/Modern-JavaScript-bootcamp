672fc55999d54fbc1d64eee215a1ca1fb5ac8d08
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-25 20:19:21 +0300

    undefined and null

---
+++ 2 functions/2-undefined-null.js
@@ -0,0 +1,26 @@
+//Undefined for variable
+let name
+name = 'Mantas'
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as function return default value
+
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+// Null has assigned value
+let age = 29
+
+age = null
+
+console.log(age)
