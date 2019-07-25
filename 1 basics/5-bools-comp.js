48921d4622ee68f18579a5aea4ef30aa8204502d
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-25 11:39:15 +0300

    bools

---
+++ 1 basics/5-bools-comp.js
@@ -0,0 +1,27 @@
+// === equality operator
+// !== not equal operator
+// < - less than operator
+// > - greater than operator
+// >= - greater than or equal operator
+// <= - less than or equal operator
+
+let temp = 32
+let isFreezing = temp >= 32
+
+console.log(isFreezing)
+
+// CHALLENGE
+
+// create age variable to your age
+// calculate is child - if they are 7 or under
+// is senior = 65 or older
+// print is child value
+// print is senior
+
+let age = 29
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
+
