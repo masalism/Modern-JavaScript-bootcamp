b895837445760d81a12509f6415d64b82b015ab4
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-07-25 10:56:20 +0300

    more variables

---
+++ 1 basics/3-variables.js
@@ -0,0 +1,13 @@
+// 1. You can't define a variable ore than twice
+let petName = 'Gojus'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+
+let a1$ = 2
+let result = 3 + 4
+
+// 3. Variables name cannot be reserved keywords
+// let let =12;
