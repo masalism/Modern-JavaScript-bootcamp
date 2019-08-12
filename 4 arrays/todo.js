d26dc60281fbe048b1391c6a0a61343bf758b03f
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-08-12 17:04:36 +0300

    array basics

---
+++ 4 arrays/todo.js
@@ -0,0 +1,7 @@
+const todos = ['Eat', 'Take a shower', 'Walk a dog', 'Have an argument', 'Master JavaScript']
+
+console.log(`You have ${todos.length} todos`)
+
+console.log(`Todo: ${todos[1]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
+
