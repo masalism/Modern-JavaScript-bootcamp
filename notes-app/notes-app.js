3be977848ed1c9b2ca880736d586e5b3d7a55974
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-08-21 16:52:39 +0300

    eventlsiteners, add, remove

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,17 +9,34 @@ const notes = [{
     body: 'Exercise. Eating a bit better'
 }]
 
-// DOM - document object model
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function(e) {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
+})
+
+
+
+// // DOM - document object model
 
 //Query and remove
 // const p = document.querySelector('p')
 // p.remove()
 
-//query all and revoe
-const ps = document.querySelectorAll('p')
+// //query all and revoe
+// const ps = document.querySelectorAll('p')
 
-ps.forEach(function (p) {
-    p.textContent = '*****'
-    // console.log(p.textContent)
-    // p.remove()
-})
+// ps.forEach(function (p) {
+//     p.textContent = '*****'
+//     console.log(p.textContent)
+//     p.remove()
+// })
+
+// //add a new element
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JS'
+// document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,11 +15,32 @@ const todos = [{
     completed: false
 }]
 
-// 1. Remove any paragraph that has text THE
-
-const paragraphs = document.querySelectorAll('p')
-paragraphs.forEach(function(paragraph) {
-    if( paragraph.textContent.includes('the')) {
-        paragraph.remove()
-    }
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 })
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
+
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log('button was clicked')
+})
+
+
+// const paragraphs = document.querySelectorAll('p')
+
+// paragraphs.forEach(function (paragraph) {
+//     if (paragraph.textContent.includes('the')) {
+//         paragraph.remove()
+//     }
+// })
+
+// Print a message how many todos left
+// Add a p off each todo left
