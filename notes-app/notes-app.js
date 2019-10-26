2e5c4a7c230abd1db7c2b04f9598b7f80da7cd13
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-10-26 14:07:38 +0300

    more dom

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,49 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function(note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+    document.querySelectorAll('.note').forEach(function (note) {
+        note.remove()
+    })
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
 // DOM - document object module
 
 // query and remove
@@ -5,10 +51,15 @@
 // p.remove()
 
 // query all and remove
-const ps = document.querySelectorAll('p')
+// const ps = document.querySelectorAll('p')
 
-ps.forEach(function (p) {
-    // p.remove()
-    // console.log(p.textContent)
-    p.textContent = '******'
-})
+// ps.forEach(function (p) {
+//     // p.remove()
+//     // console.log(p.textContent)
+//     p.textContent = '******'
+// })
+
+// Add a new element
+// const newParagraph= document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JS'
+// document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,66 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [{
+    text: 'Buy food',
+    completed: false
+}, {
+    text: 'Have an argument',
+    completed: true
+}, {
+    text: 'Master JavaScript',
+    completed: false
+}, {
+    text: 'Do nothing',
+    completed: true
+}, {
+    text: 'Apologise',
+    completed: false
+}]
 
-paragraphs.forEach(function (p) {
-    if (p.textContent.includes('the')) {
-        p.remove()
-    }
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+// listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+    console.log('Add a new todo')
 })
+
+// Lister for todo text
+document.querySelector('#new-todo-text').addEventListener('input', function (e) {
+    console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+// const paragraphs = document.querySelectorAll('p')
+
+// paragraphs.forEach(function (p) {
+//     if (p.textContent.includes('the')) {
+//         p.remove()
+//     }
+// })
