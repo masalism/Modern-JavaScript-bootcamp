8486794f1d65e3e0b2220b4aa9c535121a164f08
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-10-26 11:52:13 +0300

    dom manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,14 @@
 // DOM - document object module
+
+// query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    // p.remove()
+    // console.log(p.textContent)
+    p.textContent = '******'
+})
