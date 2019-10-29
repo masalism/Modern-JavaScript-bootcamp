74cd1472159d2d109ec245a6d5ac008511252776
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-10-29 15:25:03 +0200

    local storage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,19 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Mantas',
+//     age: 27
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -41,6 +54,18 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
 })
+
+// Create
+//localStorage.setItem('location', 'Lithuania')
+
+// Read
+// console.log( localStorage.getItem('location'))
+
+//Delete
+// localStorage.removeItem('location')
+
+//delete everything
+// localStorage.clear()
 
 // document.querySelector('#for-fun').addEventListener('change', function (e) {
 //     // console.log(e.target.checked)
