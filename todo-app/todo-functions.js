70e36f93866bc72a5344f2a7cd92e1a6822597f9
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-11-06 13:40:37 +0200

    fds

---
+++ todo-app/todo-functions.js
@@ -0,0 +1,85 @@
+// Fetch existing todos form localStorage
+const getSavedTodos = function () {
+    const todosJSON = localStorage.getItem('todos')
+
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    } else {
+        return []
+    }
+}
+
+// Save todos to local storage
+const saveTodos = function (todos) {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based of filters
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompltetedMatch = !filters.hideCompleted || !todo.completed
+        return searchTextMatch && hideCompltetedMatch
+
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+    
+    document.querySelector('#todos').innerHTML = ''
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+    generateSummaryDOM(incompleteTodos)
+
+    filteredTodos.forEach(function (todo) {
+        document.querySelector('#todos').appendChild(generateTodosDOM(todo)) 
+    })
+}
+
+// Get the DOM elements for an individual note
+const generateTodosDOM = function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    return p
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function(incompleteTodos) {
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // filteredTodos = filteredTodos.filter(function (todo) {
+    //     return !filters.hideCompleted || !todo.completed
+    //     // if (filters.hideCompleted) {
+    //     //     return !todo.completed
+    //     // } else {
+    //     //     return true
+    //     // }
+    // })
