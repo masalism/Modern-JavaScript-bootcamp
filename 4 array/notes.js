93c562ca3390551791528b580733dab214ce740b
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-01-22 10:03:53 +0200

    arrays part 1

--- 4 array/notes.js
+++ 4 array/notes.js
@@ -1,4 +1,31 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// console.log(notes.pop());
+// notes.push('My new note');
+
+// console.log(notes.shift());
+// notes.unshift('My first note');
+
+// notes.splice(1, 1, 'This is the new second item');
+
+notes[2] = 'This is now the new note 3';
+
+notes.forEach(function (item, index) {
+    console.log(index);
+    console.log(item);
+});
+
 console.log(notes.length);
-console.log(notes[notes.length - 2]);
+// console.log(notes[notes.length - 2]);
+console.log(notes);
+
+console.log(notes.indexOf('note 2'));
+
+// // Counting... 1
+// for (let i = 2; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// for (let i = notes.length - 1; i >=0; i--) {
+//     console.log(notes[i]);
+// }
--- 4 array/todo.js
+++ 4 array/todo.js
@@ -1,5 +1,19 @@
 const todos =  ['Drink', 'Eat', 'Complete this course', 'Rest', 'Go to a shower'];
 
+todos.splice(2,1);
+todos.push('Do nothing');
+todos.shift();
+
 console.log(`You have ${todos.length} todos`);
-console.log(todos[0]);
-console.log(todos[todos.length - 2]);
+
+todos.forEach(function(item, i) {
+    const num = i + 1
+    console.log(`${num}. ${item}`);
+});
+
+for (let i = 0; i < todos.length; i++) {
+    const num = i + 1
+    console.log(`${num}. ${todos[i]}`);
+}
+
+// console.log(todos);
