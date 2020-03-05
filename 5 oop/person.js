1935addc65d1d1eca2cb078ecba1caf5ae1c8913
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-03-05 09:16:40 +0200

    j

---
+++ 5 oop/person.js
@@ -0,0 +1,33 @@
+// Prototypal Inherintance
+
+'Mantas'.toLowerCase();
+
+const Person = function (firstName, lastName, age, likes =[]) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+}
+
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}`;
+
+    this.likes.forEach((like) => {
+        bio = bio + ` ${this.firstName} likes ${like}.`
+    });
+
+    return bio;
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ');
+    this.firstName = names[0];
+    this.lastName = names[1];
+}
+
+const me = new Person('Mantas', 'Masalis', 30, ['Teaching', 'Biking']);
+me.setName('Alexis Turner');
+console.log(me.getBio());
+
+const person2 = new Person('Darius', 'Tulauskas', 28);
+console.log(person2.getBio());
--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,104 +1,40 @@
-let notes = [];
+let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
-};
-
-// Check for existing saved data
-const notesJOSN = localStorage.getItem('notes') 
-
-if (notesJOSN !== null) {
-    notes = JSON.parse(notesJOSN);
-} 
-
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-    });
-
-    document.querySelector('#notes').innerHTML = '';
-
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p');
-        noteEl.textContent = note.title;
-        document.querySelector('#notes').appendChild(noteEl);
-    });
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
-renderNotes(notes, filters);
+renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
+    const id = uuidv4()
+    const timestamp = moment().valueOf()
+
     notes.push({
+        id: id,
         title: '',
-        body: ''
-    });
-    localStorage.setItem('notes',  JSON.stringify(notes));
-    renderNotes(notes, filters);
-});
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
+    })
+    saveNotes(notes)
+    location.assign(`/edit.html#${id}`)
+})
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value;
-    renderNotes(notes, filters);
-});
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value);
-});
+    filters.sortBy = e.target.value
+    renderNotes(notes, filters)
+})
 
-
-// const user = {
-//     name: 'Mantas',
-//     age: 29
-// };
-// const userJSON = JSON.stringify(user);
-// console.log(userJSON);
-// localStorage.setItem('user', userJSON);
-
-// const userJSON = localStorage.getItem('user');
-// const user = JSON.parse(userJSON);
-// console.log(user.name);
-
-//create
-// localStorage.setItem('location', 'Lithuania'); //key, value
-
-//get
-// console.log(localStorage.getItem('location'));// key
-
-//delete
-// localStorage.removeItem('location');
-
-// localStorage.clear();
-
-// document.querySelector('#for-fun').addEventListener('change', function(e) {
-//     console.log(e.target.checked);
-// });
-
-// document.querySelector('#name-form').addEventListener('submit', function(e) {
-//     e.preventDefault();
-//     console.log(e.target.elements.firstName.value);
-//     e.target.elements.firstName.value = '';
-// });
-
-
-// // DOM - Document Object Model
-
-// // const p = document.querySelector('p');
-// // p.remove();
-
-// // Query all and remove
-// const ps = document.querySelectorAll('p');
-
-// ps.forEach(function(p) {
-//     p.textContent = '*******';
-//     // console.log(p.textContent);
-// });
-
-// const newParagraph = document.createElement('p');
-// newParagraph.textContent = 'This is my new paragraph';
-// document.querySelector('body').appendChild(newParagraph);
-
-// document.querySelector('#remove-all').addEventListener('click', function (e) {
-//     document.querySelectorAll('.note').forEach(function (note) {
-//         note.remove();
-//     });
-// });
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
+})
